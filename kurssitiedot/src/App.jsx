4fc8375d434e9793ee1f3008c2226fb1c3336536
NoVittuBorje <161452390+NoVittuBorje@users.noreply.github.com>
2024-08-20 15:14:13 +0300

    Add files via upload

---
+++ kurssitiedot/src/App.jsx
@@ -0,0 +1,65 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Content = (props) => {
+  let part1=props.parts[0].name
+  let part2=props.parts[1].name
+  let part3=props.parts[2].name
+  let exercises1= props.parts[0].exercises
+  let exercises2 = props.parts[1].exercises
+  let exercises3 = props.parts[2].exercises
+
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <p>{props.total}</p>
+  )
+}
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
